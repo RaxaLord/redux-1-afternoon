20618eaed1933fea5db1b88100e7a9486d94ad14
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-04-06 13:43:45 -0700

    step 5 finished - updated Author.js / saveChanges function

--- src/store.js
+++ src/store.js
@@ -3,10 +3,14 @@ import { createStore } from 'redux';
 const initialState = {
   name: '',
   category: '',
+  authorFirst: '',
+  authorLast: '',
 };
 
 export const UPDATE_NAME = 'UPDATE_NAME';
 export const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
+export const UPDATE_AUTHOR_FIRST = 'UPDATE_FIRSTNAME';
+export const UPDATE_AUTHOR_LAST = 'UPDATE_LASTNAME';
 
 function reducer(state = initialState, action) {
   const { type, payload } = action;
@@ -17,6 +21,13 @@ function reducer(state = initialState, action) {
 
     case UPDATE_CATEGORY:
       return { ...state, category: payload };
+
+    case UPDATE_AUTHOR_FIRST:
+      return { ...state, first_name: payload };
+
+    case UPDATE_AUTHOR_LAST:
+      return { ...state, last_name: payload };
+
     default:
       return state;
   }
